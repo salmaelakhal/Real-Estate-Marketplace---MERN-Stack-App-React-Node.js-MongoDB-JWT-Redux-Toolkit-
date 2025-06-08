f545ad31704de8870a139248a22388ceda34c111
Salma Elakhal <salma.elakhal.pro@gmail.com>
2025-06-09 01:32:10 +0200

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+// Importing the environment variables from .env file
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB successfully!");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 const app = express();
 
-
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!!!!!');
+  console.log("Server is running on port 3000!!!!!");
 });
