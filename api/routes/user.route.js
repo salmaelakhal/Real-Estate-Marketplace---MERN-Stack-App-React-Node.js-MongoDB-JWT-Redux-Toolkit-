8f6d79cd58c55b0c8f67e86245a63912a869f89f
Salma Elakhal <salma.elakhal.pro@gmail.com>
2025-06-28 17:27:04 +0200

    Add contact landlord fonctioanality the listing page

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,11 @@
 import express from 'express';
-import { deleteUser, getUserListings, test, updateUser } from '../controllers/user.controller.js'; // ← ajoute .js !
+import {
+  deleteUser,
+  getUserListings,
+  test,
+  updateUser,
+  getUser,
+} from "../controllers/user.controller.js"; // ← ajoute .js !
 import { verifyToken } from '../utils/verifyUser.js'; // ← ajoute .js !
 
 
@@ -8,6 +14,7 @@ const router = express.Router();
 router.get('/test', test);
 router.put("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
-router.get('/listings/:id', verifyToken, getUserListings); 
+router.get("/listings/:id", verifyToken, getUserListings);
+router.get("/:id", verifyToken, getUser); 
 export default router;
 
